94a666a3d4f3dbcdf5111e758197014774eb91f8
Kavya Gupta <kavyagupta1609@gmail.com>
2026-07-06 16:01:29 +0530

    Fix duplicate button key error

--- src/screens/student_screen.py
+++ src/screens/student_screen.py
@@ -66,10 +66,10 @@ def student_dashboard():
 
 
         stats = stats_map.get(sid,{"total":0, "attended": 0} )
-        def unenroll_button():
-                if st.button("Unenroll from this course", type='tertiary', width='stretch', icon=':material/delete_forever:'):
+        def unenroll_button(sid=sid, sub=sub):
+                if st.button("Unenroll from this course", type='tertiary', width='stretch', icon=':material/delete_forever:', key=f"unenroll_{sid}"):
                     unenroll_student_to_subject(student_id, sid)
-                    st.toast(f'Unenrolled from {sub['name']} successfully!')
+                    st.toast(f'Unenrolled from {sub["name"]} successfully!')
                     st.rerun()
 
         with cols[i % 2]:
