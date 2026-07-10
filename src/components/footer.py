12413f267c7310f33baa2ebb83f3f94827b6ca88
Kavya Gupta <kavyagupta1609@gmail.com>
2026-07-10 16:12:02 +0530

    Update footer.py

--- src/components/footer.py
+++ src/components/footer.py
@@ -2,21 +2,9 @@ import streamlit as st
 
 
 def footer_home():
-    st.markdown("""
-        <div style="margin-top:2rem; display:flex; justify-content:center; align-items:center">
-        <p style="font-weight:600; color:#000000; font-size:0.85rem; letter-spacing:0.02em;">
-            SnapClass &middot; Made with &#10084;&#65039; by Kavya Gupta
-        </p>
-        </div>
-                """, unsafe_allow_html=True)
+    pass
 
 
 def footer_dashboard():
-    st.markdown("""
-        <div style="margin-top:2rem; display:flex; justify-content:center; align-items:center">
-        <p style="font-weight:600; color:#475569; font-size:0.85rem; letter-spacing:0.02em;">
-            SnapClass &middot; Made with &#10084;&#65039; by Kavya Gupta
-        </p>
-        </div>
-                """, unsafe_allow_html=True)
+    pass
 
